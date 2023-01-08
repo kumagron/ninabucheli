f9181f80c37e1a425f855deee3bac01fe29df579
Jeffrey Sulit <Jeffrey.Sulit@shl-group.com>
2023-01-08 19:49:56 +0100

    Updated Contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -12,20 +12,31 @@
                         <div class="div-block-2">
                             <h3>Termin buchen</h3>
                             
-                            <p><strong>Zeitfenster:</strong><br>DI und FR jeweils morgens 09:30-11:00</p>
-                            <p><strong>Preis:</strong><br>CHF 130 pro Stunde</p>
                             <p>
-                                <strong>Zahlungsmöglichkeiten:</strong><br>
-                                Per TWINT oder bar
+                                <strong>Zeitfenster und Ort:</strong>
+                                <br>DI, MI und FR jeweils morgens 09:00-11:15 Uhr
+                                <br>
+                                <br>Online per ZOOM oder einfach per Telefon,
+                                <br>vor Ort in Neuenkirch ab Januar 2023
+                            </p>
+
+                            <p>
+                                <strong>Preis:</strong>
+                                <br>CHF 130 pro Stunde
                             </p>
                             <p>
-                                <strong>Zur Info:</strong><br>
-                                Ab 1 Stunde rechne ich im 15 Minuten-Takt ab.<br>
-                                Die Coaching sind von den Krankenkassen noch nicht anerkannt.<br>
+                                <strong>Zahlungsmöglichkeiten:</strong>
+                                <br>Bar oder per TWINT
+                            </p>
+                            <p>
+                                <strong>Zur Info:</strong>
+                                <br>Ab 1 Stunde rechne ich im 15 Minuten-Takt ab.
+                                <br>Die Coaching sind von den Krankenkassen noch nicht anerkannt.
                             </p>
                             
                             <p>
-                                 Bitte kontaktiere mich 24 Stunden im Voraus, falls du den Termin stornieren oder verschieben möchtest.
+                                <br>Bitte kontaktiere mich 24 Stunden im Voraus, falls du den Termin stornieren oder verschieben möchtest.
+                                <br>Für Nichtkontaktieren, erlaube ich mir 130 CHF in Rechnung zu stellen.
                             </p>
                         </div>
                     </div>
